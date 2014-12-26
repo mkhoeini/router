0eff77851d4f6ee2bf5b5004c2366daae677ca7f
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2014-12-26 12:41:03 +0330

    Implemented the first version

---
+++ src/router/core.clj
@@ -0,0 +1,9 @@
+(ns router.core
+  (:require [secretary.core :as sty :include-macroes true]))
+
+
+
+(defmacro match
+  "Match a route and return a value."
+  [route value]
+  `(sty/defroute ~route {:as p#} {:value ~value :params p#}))
---
+++ src/router/core.cljs
@@ -0,0 +1,85 @@
+(ns router.core
+  (:require [secretary.core :as secretary]
+            [goog.history.EventType :as EventType]
+            [goog.events :as events])
+  (:import [goog.history Html5History]
+           [goog Uri]))
+
+
+
+;; The main history manager
+(def history (Html5History.))
+
+
+
+;; Default configurations for history
+(def default-config
+  {:use-fragment false
+   :path-prefix ""
+   :enabled true})
+
+
+
+(defn config!
+  "Config the history manager."
+  ([] (config! default-config))
+  ([c]
+   (let [conf (merge default-config c)]
+     (doto history
+       (.setUseFragment (:use-fragment conf))
+       (.setPathPrefix (:path-prefix conf))
+       (.setEnabled (:enabled conf))))))
+
+
+
+(defn enable-router!
+  "Enable or disable the history manager."
+  ([] (enable-router! true))
+  ([en?] (.setEnabled history en?)))
+
+(defn disable-router! []
+  (enable-router! false))
+
+
+
+;; The callback to be called when navigation happens
+(def ^:private callback (atom #(do nil)))
+
+(defn set-callback!
+  "Set the callback function for navigation event."
+  [cb]
+  (reset! callback cb))
+
+
+
+;; Listen to navigation events and call the callback
+(events/listen history EventType/NAVIGATE
+               (fn [nav]
+                 (let [param (secretary/dispatch! (.-token nav))]
+                   (@callback param))))
+
+
+
+(defn current-route
+  "Return the current route"
+  []
+  (.getToken history))
+
+(defn navigate!
+  "Go to a new route"
+  [nr]
+  (.setToken history nr))
+
+
+
+(defn prevent-default-refresh!
+  "Prevent links from redirecting."
+  []
+  (events/listen js/document "click"
+                 (fn [e]
+                   (let [path (->> (.. e -target -href)
+                                   (.parse Uri)
+                                   (.getPath))]
+                     (when (secretary/locate-route path)
+                       (.setToken history path)
+                       (.preventDefault e))))))
