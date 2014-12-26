923e2085f59f1ecec74959ef0db3b29a64b09b11
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2014-12-26 12:40:03 +0330

    Wrote the README

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject org.clojars.mkhoeini/router "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://www.github.com/mkhoeini/router"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [secretary  "1.2.1"]])
