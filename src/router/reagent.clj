196a5aa5d2ed8c0d949ebdeda4765bb47a5418c3
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2015-01-05 20:36:26 +0330

    Fixed a misspelling

--- src/router/reagent.clj
+++ src/router/reagent.clj
@@ -1,11 +1,10 @@
 (ns router.reagent
-  (:require [router.core :as rt]
-            [reagent.core :as reagent]))
+  (:require [router.core :as rt]))
 
 
 
 (defmacro match
   "An alternative to router.core/match to fasilitate Reagent."
   [route param elem]
-  (let [f `(fn [~param] (reagent/as-element ~elem))]
+  (let [f `(fn [~param] (~'reagent.core/as-element ~elem))]
     `(rt/match ~route ~f)))
