ef4b789119c09381a878212aab6e827a6e50dc4b
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2015-01-05 20:03:21 +0330

    Updated the project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject org.clojars.mkhoeini/router "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "An HTML5 router for Clojurescript, React, and Reagent."
   :url "http://www.github.com/mkhoeini/router"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
+
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [reagent "0.5.0-alpha"]
+                 [reagent "0.5.0-alpha" :scop "provided"]
                  [secretary  "1.2.1"]])
